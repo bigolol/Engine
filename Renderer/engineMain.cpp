21a6eabf5e2bc68b633df817563181622df768d6
Holger Klein <holgerhugoklein@gmail.com>
2015-04-30 22:55:06 +0200

    added setup and cleanup methods to sdl

--- Renderer/engineMain.cpp
+++ Renderer/engineMain.cpp
@@ -1,11 +1,22 @@
 #include "engineMain.h"
 
 
-EngineMain::EngineMain()
+EngineMain::EngineMain(int width, int height, char *title) : m_iHeight(height), m_iWidth(width), m_sTitle(title)
 {
 }
 
 
 EngineMain::~EngineMain()
 {
+}
+
+void EngineMain::init()
+{
+	m_window = SDL_CreateWindow(m_sTitle, SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, m_iWidth, m_iHeight, SDL_WINDOW_OPENGL;
+}
+
+void EngineMain::cleanUp()
+{
+	SDL_DestroyWindow(m_window);
 }
--- Renderer/engineMain.h
+++ Renderer/engineMain.h
@@ -1,16 +1,22 @@
 #pragma once
+#include <SDL2\SDL.h>
 class EngineMain
 {
 public:
-	EngineMain();
+	EngineMain(int width, int height, char *title);
 	~EngineMain();
 	void init();
 	void cleanUp();
 private:
 	EngineMain operator=(const EngineMain& rhs);
 	EngineMain(const EngineMain& rhs);
+
 public:
 
 private:
+	int m_iWidth;
+	int m_iHeight;
+	const char *m_sTitle;
+	SDL_Window *m_window;
 };
 
